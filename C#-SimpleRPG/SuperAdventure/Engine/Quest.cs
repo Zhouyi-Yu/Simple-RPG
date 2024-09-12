8761100db884dd23c48379811235edade35c8d5c
Zhouyi Yu <zhouyi1@ualberta.ca>
2024-09-12 17:00:59 -0600

    11.1

--- C#-SimpleRPG/SuperAdventure/Engine/Quest.cs
+++ C#-SimpleRPG/SuperAdventure/Engine/Quest.cs
@@ -13,6 +13,7 @@ namespace Engine
         public string Description { get; set; }
         public int RewardExperiencePoints {  get; set; }
         public int RewardGold {  get; set; }
+        public List<QuestCompletionItem> QuestCompletionItems {  get; set; } 
 
         public Quest(int id, string name, string description, int rewardExperiencePoints, int rewardGold)
         {
@@ -21,6 +22,7 @@ namespace Engine
             Description = description;
             RewardExperiencePoints = rewardExperiencePoints;
             RewardGold = rewardGold;
+            QuestCompletionItems = new List<QuestCompletionItem>();
         }
 
     }
